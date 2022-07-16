cac567964cd152f020c6e6c972ee0ea1932eb215
Ayanesh <ayaneshsarkar101@gmail.com>
2022-07-16 13:57:06 +0530

    Initial Commit :heavy_check_mark:

---
+++ max-project-three/pages/index.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import fs from 'fs/promises';
+import Link from 'next/link';
+
+const HomePage = (props) => {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products &&
+        products.length &&
+        products.map((product) => (
+          <li key={product.id}>
+            <Link href={`/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+export const getStaticProps = async () => {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: { destination: '/no-data' },
+    };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+};
+
+export default HomePage;
